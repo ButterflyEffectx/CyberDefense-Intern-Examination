6a69da8120a73d81fc2b1eff2173ad1f17af2108
ButterflyEffectx <onyoux755@gmail.com>
2025-11-26 11:30:45 +0700

    package marker

---
+++ backend/app/__init__.py
@@ -0,0 +1,16 @@
+import os 
+from opensearchpy import OpenSearch, RequestsHttpConnection
+
+def get_client():
+    host = os.getenv("OPENSEARCH_HOST", "opensearch")
+    port = int(os.getenv("OPENSEARCH_PORT", "9200"))
+    use_ssl = os.getenv("OPENSEARCH_USE_SSL", "false").lower() == "true"
+
+    client = OpenSearch(
+        hosts=[{'host': host, 'port': port}],
+        http_compass = True,
+        use_ssl=use_ssl,
+        verify_certs = False,
+        connection_class=RequestsHttpConnection,
+    )
+    return client
